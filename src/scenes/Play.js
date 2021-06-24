644a6d235a9c36b60b82b62a9f2bfc49b7487dc2
Yuhao <yuhao.chen2000@icloud.com>
2021-06-24 19:21:05 +0000

    added spaceship into the scene

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,6 +11,12 @@ class Play extends Phaser.Scene {
 	create() {
 		// place tile sprite
 		this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+		// add rocket (p1)
+		this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+		// add spaceships
+		this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize * 4, 'spaceship', 0, 30).setOrigin(0, 0);
+		this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize * 5 + borderPadding * 2, 'spaceship', 0, 20).setOrigin(0, 0);
+		this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding * 4, 'spaceship', 0, 10).setOrigin(0, 0);
 		// green UI background
 		this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2, 0x37946e).setOrigin(0, 0);
 		// white borders
@@ -18,8 +24,6 @@ class Play extends Phaser.Scene {
 		this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
 		this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
 		this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-		// add rocket (p1)
-		this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
 		// define keys 
 		keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
 		keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
@@ -29,5 +33,8 @@ class Play extends Phaser.Scene {
 	update(){
 		this.starfield.tilePositionX -= 5;
 		this.p1Rocket.update();
+		this.ship01.update();
+		this.ship02.update();
+		this.ship03.update();
 	}
 }
