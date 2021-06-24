541f32b5151457ba8a5a9e56384389d962891ba5
Yuhao <yuhao.chen2000@icloud.com>
2021-06-24 18:45:14 +0000

    added rocket into the game

--- src/scenes/Rocket.js
+++ src/scenes/Rocket.js
@@ -1,5 +1,5 @@
 // Rocket prefab
-class rocket extends Phaser.GameObjects.Sprite {
+class Rocket extends Phaser.GameObjects.Sprite {
 	constructor(scene, x, y, texture, frame) {
 		super(scene, x, y, texture, frame);
 		
