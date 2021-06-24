2308f6a5162d5806976bb57948c925422044fb6a
Yuhao <yuhao.chen2000@icloud.com>
2021-06-24 19:00:33 +0000

    added basic movement and key binding

--- src/scenes/Rocket.js
+++ src/scenes/Rocket.js
@@ -5,5 +5,26 @@ class Rocket extends Phaser.GameObjects.Sprite {
 		
 		// add object to existing scene
 		scene.add.existing(this);
+		this.isFiring = false;
+		this.moveSpeed = 2;
+	}
+	update() {
+		// left/right movement
+		if(!this.isFiring) {
+			if(keyLEFT.isDown && this.x >= borderUISize + this.width){
+				this.x -= this.moveSpeed;
+			} else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width) {
+				this.x += this.moveSpeed;
+			}
+		}
+		// fire button
+		if(Phaser.Input.Keyboard.JustDown(keyF)) {
+			this.isFiring;
+		}
+		// rest on miss
+		if(this.y <= borderUISize * 3 + borderPadding) {
+			this.isFiring = false;
+			this.y = game.config.height - borderUISize - borderPadding;
+		}
 	}
 }
