167a3c1ad395988f39756d57686e3dd7b9848bf4
Yuhao <yuhao.chen2000@icloud.com>
2021-06-25 06:39:26 +0000

    added score keeping

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -36,7 +36,23 @@ class Play extends Phaser.Scene {
 			key: 'explode',
 			frames: this.anims.generateFrameNumbers('explosion', { start:0, end: 9, first: 0}),
 			frameRate: 30
-		})
+		});
+		// initialize score
+		this.p1Score = 0;
+		// display score
+		let scoreConfig = {
+			fontFamily: 'Courier',
+			fontSize: '28px',
+			backgroundColor: '#F3B141',
+			color: '#843605',
+			align: 'right',
+			padding: {
+				top: 5,
+				bottom: 5,
+			},
+			fixedWidth: 100
+		};
+		this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding * 2, this.p1Score, scoreConfig);
 	}
 	update(){
 		this.starfield.tilePositionX -= 5;
@@ -80,6 +96,9 @@ class Play extends Phaser.Scene {
 			ship.reset();
 			ship.alpha = 1;
 			boom.destroy();
-		})
+		});
+		// score add and repaint
+		this.p1Score += ship.points;
+		this.scoreLeft.text = this.p1Score;
 	}
 }
