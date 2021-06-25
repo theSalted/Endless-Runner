25081db3dfa6a92d9984b6be7405cb118317f1a6
Yuhao <yuhao.chen2000@icloud.com>
2021-06-25 04:46:40 +0000

    remove unessary assets

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -40,17 +40,14 @@ class Play extends Phaser.Scene {
 		
 		// check collisions
 		if(this.checkCollison(this.p1Rocket, this.ship03)) {
-			console.log('kaboom ship 03')
 			this.p1Rocket.reset();
 			this.ship03.reset();
 		}
 		if(this.checkCollison(this.p1Rocket, this.ship02)) {
-			console.log('kaboom ship 02')
 			this.p1Rocket.reset();
 			this.ship02.reset();
 		}
 		if(this.checkCollison(this.p1Rocket, this.ship01)) {
-			console.log('kaboom ship 01')
 			this.p1Rocket.reset();
 			this.ship01.reset();
 		}
