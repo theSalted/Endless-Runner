c57893a613d1d077a6a81a659afee4b7b2ded01d
Yuhao <yuhao.chen2000@icloud.com>
2021-06-25 15:13:05 +0000

    fixes

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -78,7 +78,7 @@ class Play extends Phaser.Scene {
 		}, null, this);
 	}
 	update(){
-		console.log(this.clock.SECOND);
+		//console.log(this.clock.SECOND);
 		if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
 			this.scene.restart();
 		}
