74f89ca34992f5101241bea7b9bcab1b0fb2421c
Yuhao <yuhao.chen2000@icloud.com>
2021-06-25 07:59:57 +0000

    added sfx and loaded them

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,6 +2,12 @@ class Menu extends Phaser.Scene {
 	constructor() {
 		super("menuScene")
 	}
+	preload() {
+		// load audio
+		this.load.audio('sfx_select', './assets/blip.wav');
+		this.load.audio('sfx_explosion', './assets/explosion.wav');
+		this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
+	}
 	create() {
 		this.add.text(20, 20, "Rocket Patrol Menu")
 		this.scene.start("playScene")
