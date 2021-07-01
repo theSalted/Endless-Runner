6725f2c6030e72121c87c05b8bc6a9d3a69c0569
dearsunny499 <86376909+dearsunny499@users.noreply.github.com>
2021-07-01 13:43:18 +0000

    update bear explode animation

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -11,7 +11,7 @@ class Play extends Phaser.Scene {
 		this.load.image('mounts', './assets/mounts.png');
 		this.load.image('clouds', './assets/cloud.png');
 		this.load.image('starfield', './assets/starfield.png');
-		this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9})
+		this.load.spritesheet('explosion', './assets/bearexplode.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9})
 		this.load.spritesheet('rollingBear', './assets/bearsheet.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 4})
 	}
 	create() {
@@ -25,7 +25,7 @@ class Play extends Phaser.Scene {
 		this.anims.create({
 			key: 'explode',
 			frames: this.anims.generateFrameNumbers('explosion', { start:0, end: 9, first: 0}),
-			frameRate: 30
+			frameRate: 24
 		});
 		
 		this.anims.create({
